7816adcd55dda254206946ba9193d4c0be5cc89d
ibnekaeysh <ibnekayesh91@gmail.com>
2022-10-07 13:04:45 +0600

    Add project files.

---
+++ ViewPages/Products.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace SievePOS.ViewPages
+{
+    /// <summary>
+    /// Interaction logic for Products.xaml
+    /// </summary>
+    public partial class Products : Page
+    {
+        public Products()
+        {
+            InitializeComponent();
+        }
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9.-]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Submit Successfully", this.Title);
+        }
+    }
+}
